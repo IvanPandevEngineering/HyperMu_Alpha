793f64052ecc9050a4e50c35e76355a5ad34ec50
IvanPandevEngineering <131015810+IvanPandevEngineering@users.noreply.github.com>
2023-06-19 16:27:36 -0700

    file structure updates

--- sample_usage.py
+++ sample_usage.py
@@ -2,19 +2,19 @@ import vehicle as v
 import condition_data as cdata
 import matplotlib.pyplot as plt
 
-BB = v.vehicle('Battle_Bimmer_28_Dec_2022.yml')
+BB = v.vehicle(r'sample_vehicles\\Battle_Bimmer_28_Dec_2022.yml')
 
 #BB.summary()
 
-#BB.plot_shaker(
-#    replay_src = r'sample_data\\12_March_2023\\SensorLogFiles_my_iOS_device_230314_07-21-07\\2023-03-12_14_34_29_my_iOS_device.csv'
-#)
-
-BB.correlation_check(
-    replay_src = r'sample_data\\12_March_2023\\SensorLogFiles_my_iOS_device_230314_07-21-07\\2023-03-12_14_34_29_my_iOS_device.csv'
+BB.plot_shaker(
+    replay_src = r'sample_telemetry\\12_March_2023\\2023-03-12_14_34_29_my_iOS_device.csv'
 )
+
+#BB.correlation_check(
+#    replay_src = r'sample_telemetry\\12_March_2023\\2023-03-12_14_34_29_my_iOS_device.csv'
+#)
 
 
 #BB.correlation_check(
-#    replay_src = r'sample_data\\16_June_2023\\2023-06-16_18_50_04_my_iOS_device.csv'
+#    replay_src = r'sample_telemetry\\16_June_2023\\2023-06-16_18_50_04_my_iOS_device.csv'
 #)
--- visualizer.py
+++ visualizer.py
@@ -9,7 +9,7 @@ roll_angle_f, roll_angle_r, pitch_angle):
     print('Graphing...')
 
     plt.style.use('seaborn-v0_8')
-    fig, subplots = plt.subplots(2, 3, figsize=(14, 8))
+    fig, subplots = plt.subplots(3, 2, figsize=(14, 8))
     fig.suptitle('Race Telemetry on Battle_Bimmer_28_Dec_2022', fontsize=14)
 
     subplots[0,0].plot(force_function['loggingTime(txt)'], force_function['accelerometerAccelerationX(G)'], label='lateral accel (G)')
@@ -25,28 +25,28 @@ roll_angle_f, roll_angle_r, pitch_angle):
     subplots[0,1].legend()
     subplots[0,1].grid(True)
 
-    subplots[0,2].plot(force_function['loggingTime(txt)'], damper_vel_fr, label='damper speed (m/s, fr)')
-    subplots[0,2].plot(force_function['loggingTime(txt)'], damper_vel_fl, label='damper speed (m/s, fl)')
-    subplots[0,2].plot(force_function['loggingTime(txt)'], damper_vel_rr, label='damper speed (m/s, rr)')
-    subplots[0,2].plot(force_function['loggingTime(txt)'], damper_vel_rl, label='damper speed (m/s, rl)')
-    subplots[0,2].legend()
-    subplots[0,2].grid(True)
-
-    subplots[1,0].plot(force_function['loggingTime(txt)'], tire_load_fr, label='tire load (N, fr)')
-    subplots[1,0].plot(force_function['loggingTime(txt)'], tire_load_fl, label='tire load (N, fl)')
-    subplots[1,0].plot(force_function['loggingTime(txt)'], tire_load_rr, label='tire load (N, rr)')
-    subplots[1,0].plot(force_function['loggingTime(txt)'], tire_load_rl, label='tire load (N, rl)')
+    subplots[1,0].plot(force_function['loggingTime(txt)'], damper_vel_fr, label='damper speed (m/s, fr)')
+    subplots[1,0].plot(force_function['loggingTime(txt)'], damper_vel_fl, label='damper speed (m/s, fl)')
+    subplots[1,0].plot(force_function['loggingTime(txt)'], damper_vel_rr, label='damper speed (m/s, rr)')
+    subplots[1,0].plot(force_function['loggingTime(txt)'], damper_vel_rl, label='damper speed (m/s, rl)')
     subplots[1,0].legend()
     subplots[1,0].grid(True)
 
-    subplots[1,1].plot(force_function['loggingTime(txt)'], lateral_load_dist_f, label='lateral load dist (%, f)')
-    subplots[1,1].plot(force_function['loggingTime(txt)'], lateral_load_dist_r, label='lateral load dist (%, r)')
+    subplots[1,1].plot(force_function['loggingTime(txt)'], tire_load_fr, label='tire load (N, fr)')
+    subplots[1,1].plot(force_function['loggingTime(txt)'], tire_load_fl, label='tire load (N, fl)')
+    subplots[1,1].plot(force_function['loggingTime(txt)'], tire_load_rr, label='tire load (N, rr)')
+    subplots[1,1].plot(force_function['loggingTime(txt)'], tire_load_rl, label='tire load (N, rl)')
     subplots[1,1].legend()
     subplots[1,1].grid(True)
 
-    subplots[1,2].plot(force_function['loggingTime(txt)'], lateral_load_dist_f/(lateral_load_dist_r+lateral_load_dist_f), label='lateral load dist ratio (%, f)')
-    subplots[1,2].legend()
-    subplots[1,2].grid(True)
+    subplots[2,0].plot(force_function['loggingTime(txt)'], lateral_load_dist_f, label='lateral load dist (%, f)')
+    subplots[2,0].plot(force_function['loggingTime(txt)'], lateral_load_dist_r, label='lateral load dist (%, r)')
+    subplots[2,0].legend()
+    subplots[2,0].grid(True)
+
+    subplots[2,1].plot(force_function['loggingTime(txt)'], lateral_load_dist_f/(lateral_load_dist_r+lateral_load_dist_f), label='lateral load dist ratio (%, f)')
+    subplots[2,1].legend()
+    subplots[2,1].grid(True)
 
     fig.tight_layout()
     plt.show()
